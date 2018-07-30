aadffebf1a538d3c9d49cad176a5282b0a4a44e8
eseunghwan <reignstarbits@gmail.com>
2018-07-30 20:15:42 +0900

    Update __init__.py

--- pybillboard_js/__init__.py
+++ pybillboard_js/__init__.py
@@ -5,6 +5,10 @@ from tinydb import TinyDB, Query
 # import logger
 from .logger import Logger
 
+if not os.path.exists(os.path.join(module_dir, "res")):
+    os.mkdir(os.path.join(module_dir, "res"))
+    TinyDB(os.path.join(module_dir, "res", "config.db"), default_table = "config", indent = 4).insert({"NAME": "UPDATE_RES", "VALUE": True})
+
 config, query = TinyDB(os.path.join(module_dir, "res", "config.db"), default_table = "config", indent = 4), Query()
 
 if config.search(query.NAME == "UPDATE_RES")[0]["VALUE"]:# check for update res
